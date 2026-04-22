26afc806b193c1a9f5dc1249b188f7924bb2d3d5
Joamerson Islan <joamersonislan38@gmail.com>
2026-04-21 21:45:15 -0300

    feat: implementacao do insertion sort

---
+++ algoritmos/insertion_sort.cpp
@@ -0,0 +1,21 @@
+#include "algorithms.h"
+#include <cstdlib>
+#include <vector>
+
+void insertionSort(int n) {
+  std::vector<int> arr(n);
+  for (int i = 0; i < n; i++) {
+    arr[i] = rand() % n;
+  }
+
+  for (int i = 1; i < n; i++) {
+    int key = arr[i];
+    int j = i - 1;
+
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = key;
+  }
+}
